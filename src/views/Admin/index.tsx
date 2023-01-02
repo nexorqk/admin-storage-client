25f61212d54282324de9596412a6b85b68880870
Siarhei <sergmlynchyk@gmail.com>
2023-01-02 23:32:20 +0300

    add sidebar

---
+++ src/views/Admin/index.tsx
@@ -0,0 +1,12 @@
+import Panel from '../Panel'
+import styles from './Admin.module.scss'
+
+const Admin = () => {
+    return (
+        <div className={styles.wrapper}>
+            <Panel />
+        </div>
+    )
+}
+
+export default Admin
