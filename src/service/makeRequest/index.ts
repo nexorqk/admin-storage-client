db7cbaa8e5b286325f51dbeda1ff0dd8f249f7d1
Siarhei <sergmlynchyk@gmail.com>
2023-01-13 20:42:37 +0300

    add axios

---
+++ src/service/makeRequest/index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+interface IMakeRequest {
+    url: string
+    data?: any
+    method?: 'get' | 'post'
+    params?: any
+    headers?: any
+}
+
+export const makeRequest = async ({
+    url,
+    method = 'get',
+    data,
+    params,
+    headers,
+}: IMakeRequest) => {
+    return await axios({
+        url,
+        method,
+        data,
+        headers,
+        params,
+    } as IMakeRequest)
+}
